0b7d01fb7664fd9acdb4297e6b5c9f6cec39b79c
nmarkro <nmarkro@gmail.com>
2020-06-25 13:30:21 -0500

    whoops

--- ListExercises/Program.cs
+++ ListExercises/Program.cs
@@ -8,19 +8,22 @@ namespace ListExercises
         {
             Console.WriteLine("Hello Lists!");
             int[] numList = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            Console.WriteLine(Sum(numList));
+            Console.WriteLine(SumEven(numList));
             string[] words = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse".Split(" ");
             Console.Write("Input selected word length: ");
             int input = int.Parse(Console.ReadLine());
             PrintFilteredByLength(words, input);
         }
 
-        static int Sum(int[] nums)
+        static int SumEven(int[] nums)
         {
             int sum = 0;
             foreach(int i in nums)
             {
-                sum += i;
+                if (i % 2 == 0)
+                {
+                    sum += i;
+                }
             }
             return sum;
         }
